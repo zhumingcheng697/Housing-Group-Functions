1a2f7146fa432c260a4e2625fbfbf4945d700221
McCoy Zhu <mz1945@nyu.edu>
2021-01-14 11:19:37 -0800

    ADD: default PHP mail func

--- mail-api.php
+++ mail-api.php
@@ -42,13 +42,25 @@ function send_email($sender_name, $sender_email, $recipient, $subject, $body) {
     $props = array();
     parse_str($_SERVER['QUERY_STRING'], $props);
 
-    $sender_name = $props["name"] ?? null;
-    $sender_email = $props["email"] ?? null;
+    $sender_name = ($props["name"] ?? null) ? $props["name"] : "PHP Email Lambda Test";
+    $sender_email = ($props["email"] ?? null) ? $props["email"] : "nyu-dining-test@outlook.com";
     $recipient = ($props["to"] ?? null) ? $props["to"] : (($props["mailto"] ?? null) ? $props["mailto"] : "nyu-dining-test@outlook.com");
     $subject = ($props["subject"] ?? null) ? $props["subject"] : "PHP Email Lambda Test (" . date(DATE_RFC2822) . ")";
     $body = ($props["body"] ?? null) ? $props["body"] : (($props["msg"] ?? null) ? $props["msg"] : (($props["message"] ?? null) ? $props["message"] : "This is an automatic email sent using PHP Lambda."));
 
     send_email($sender_name, $sender_email, $recipient, $subject, $body);
+
+    echo "Sending through default mail\n";
+
+    ini_set("sendmail_from", $sender_email);
+
+    if (mail($recipient, $subject, $body, "From: " . $sender_name . " <" . $sender_email . ">")) {
+        echo "Email sent successfully to \"" . $recipient . "\"\n";
+    } else {
+        echo "Email failed to send\n";
+    }
+
+    ini_restore("sendmail_from");
 })();
 
 ?>
