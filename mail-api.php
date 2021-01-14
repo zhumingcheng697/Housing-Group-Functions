664629648f200c31b648fd9f4ba496b4dd2da08b
McCoy Zhu <mz1945@nyu.edu>
2021-01-14 15:01:31 -0800

    ADD: fallback and default parameters

--- mail-api.php
+++ mail-api.php
@@ -2,8 +2,6 @@
 
 require __DIR__ . '/mail-helper.php';
 
-(function() {
-    mail_helper(null, null, null, null, null);
-})();
+mail_helper();
 
 ?>
--- mail-helper.php
+++ mail-helper.php
@@ -37,15 +37,15 @@ function send_email($sender_name, $sender_email, $recipient, $subject, $body) {
     }
 }
 
-function mail_helper($default_sender_name, $default_sender_email, $default_recipient, $default_subject, $default_body) {
+function mail_helper($overloaded_sender_name = null, $overloaded_sender_email = null, $overloaded_recipient = null, $overloaded_subject = null, $overloaded_body = null, $fallback_sender_name = null, $fallback_sender_email = null, $fallback_recipient = null, $fallback_subject = null, $fallback_body = null) {
     $props = array();
     parse_str($_SERVER['QUERY_STRING'], $props);
 
-    $sender_name = $default_sender_name ? $default_sender_name : (($props["name"] ?? null) ? $props["name"] : (($props["sender"] ?? null) ? $props["sender"] : "PHP Email Lambda Test"));
-    $sender_email = $default_sender_email ? $default_sender_email : (($props["email"] ?? null) ? $props["email"] : (($props["from"] ?? null) ? $props["from"] : "nyu-dining-test@outlook.com"));
-    $recipient = $default_recipient ? $default_recipient : (($props["to"] ?? null) ? $props["to"] : (($props["mailto"] ?? null) ? $props["mailto"] : "nyu-dining-test@outlook.com"));
-    $subject = $default_subject ? $default_subject : (($props["subject"] ?? null) ? $props["subject"] : "PHP Email Lambda Test (" . date(DATE_RFC2822) . ")");
-    $body = $default_body ? $default_body : (($props["body"] ?? null) ? $props["body"] : (($props["msg"] ?? null) ? $props["msg"] : (($props["message"] ?? null) ? $props["message"] : "This is an automatic email sent using PHP Lambda.")));
+    $sender_name = $overloaded_sender_name ? $overloaded_sender_name : (($props["name"] ?? null) ? $props["name"] : (($props["sender"] ?? null) ? $props["sender"] : ($fallback_sender_name ? $fallback_sender_name : "PHP Email Lambda Test")));
+    $sender_email = $overloaded_sender_email ? $overloaded_sender_email : (($props["email"] ?? null) ? $props["email"] : (($props["from"] ?? null) ? $props["from"] : ($fallback_sender_email ? $fallback_sender_email : "nyu-dining-test@outlook.com")));
+    $recipient = $overloaded_recipient ? $overloaded_recipient : (($props["to"] ?? null) ? $props["to"] : (($props["mailto"] ?? null) ? $props["mailto"] : ($fallback_recipient ? $fallback_recipient : "nyu-dining-test@outlook.com")));
+    $subject = $overloaded_subject ? $overloaded_subject : (($props["subject"] ?? null) ? $props["subject"] : ($fallback_subject ? $fallback_subject : "PHP Email Lambda Test (" . date(DATE_RFC2822) . ")"));
+    $body = $overloaded_body ? $overloaded_body : (($props["body"] ?? null) ? $props["body"] : (($props["msg"] ?? null) ? $props["msg"] : (($props["message"] ?? null) ? $props["message"] : ($fallback_body ? $fallback_body : "This is an automatic email sent using PHP Lambda."))));
 
     send_email($sender_name, $sender_email, $recipient, $subject, $body);
 }
