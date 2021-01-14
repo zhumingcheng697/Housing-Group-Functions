5412f909bac80fb4405c67ddc796423e2682066f
McCoy Zhu <mz1945@nyu.edu>
2021-01-14 15:04:15 -0800

    ADD: post sublet offer script

---
+++ post-sublet-offer.php
@@ -0,0 +1,10 @@
+<?php
+
+require __DIR__ . '/mail-helper.php';
+
+mail_helper(
+    null, null, "faculty-housing-test-external-group@googlegroups.com", null, null,
+    null, null, null, "New Sublet Offer", null
+);
+
+?>
