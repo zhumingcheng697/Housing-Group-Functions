1a438460591cdb8f4fa68798ff4d8faba4811683
McCoy Zhu <mz1945@nyu.edu>
2021-01-15 20:31:42 -0800

    UPDATE: post-offer.php and post-offer-demo.php

--- post-offer.php
+++ post-offer.php
@@ -2,9 +2,62 @@
 
 require __DIR__ . '/mail-helper.php';
 
-mail_helper(
-    null, null, "faculty-housing-test-external-group@googlegroups.com", null, null,
-    null, null, null, "New Sublet Offer", null
+$props = array();
+parse_str($_SERVER['QUERY_STRING'], $props);
+
+function tableRow($key, $value) {
+    return "<tr><td style='padding: 5px 15px 5px 0px; vertical-align: top;'><b>" . htmlentities($key) . ":</b></td><td style='padding: 5px 0; vertical-align: top;'>" . htmlentities($value) . "</td></tr>";
+}
+
+function table(...$rows) {
+    $tableStr = "<table>";
+    foreach ($rows as &$row) {
+        if (($row["key"] ?? $row[0]) && ($row["value"] ?? $row[1])) {
+            $tableStr .= tableRow($row["key"] ?? $row[0], str_replace(["\n", "\r"], "</br>", $row["value"] ?? $row[1]));
+        }
+    }
+    $tableStr .= "</table>";
+    return $tableStr;
+}
+
+$email_msg = "";
+
+$email_msg .= "<h2>" . "Contact Information" . "</h2>";
+$email_msg .= table(
+    ["Full Name", $props["name"]],
+    ["Department", $props["department"]],
+    ["Phone", $props["phone"]],
+    ["Email", $props["email"]]
+);
+$email_msg .= "</br>";
+
+$email_msg .= "<h2>" . "Sublet Term" . "</h2>";
+$email_msg .= table(
+    ["Start Date (mm/dd/yyyy)", $props["start_date"]],
+    ["End Date (mm/dd/yyyy)", $props["end_date"]]
+);
+$email_msg .= "</br>";
+
+$email_msg .= "<h2>" . "Unit Information" . "</h2>";
+$email_msg .= table(
+    ["Address", $props["address"]],
+    ["Number of Bedrooms", $props["num_of_bedroom"]],
+    ["Number of Bathrooms", $props["num_of_bathroom"]],
+    ["Current Monthly Rent", $props["rent"]],
+    ["Current Monthly Utilities Charges", $props["utilities"]],
+    ["Current Monthly Cable/Internet/Phone Charges", $props["network"]],
+    ["No Smoking", $props["no_smoking"]],
+    ["No Pets", $props["no_pets"]],
+    ["Water Plants", $props["water_plants"]],
+    ["Some closets will be reserved for storage", $props["reserved_closets"]]
+);
+
+send_email(
+    $props["name"],
+    $props["email"],
+    (($props["dev"] ?? null) ? (($props["dev_email"] ?? null) ? $props["dev_email"] : "nyu-faculty-housing-group@outlook.com") : "faculty-housing-test-external-group@googlegroups.com"),
+    "New Sublet Offer",
+    $email_msg
 );
 
 ?>
