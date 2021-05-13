d4ed1a089996b7f6029dffd7d272b4173a89f516
McCoy Zhu <mz1945@nyu.edu>
2021-05-13 12:56:40 -0700

    UPDATE: add isset test

--- mail-helper.php
+++ mail-helper.php
@@ -20,8 +20,11 @@ function get_email_config($callback) {
 
         if (isset($result['SecretString'])) {
             $callback(json_decode($result['SecretString'], true));
-        } else {
+        } else if (isset($result['SecretBinary'])) {
             $callback(json_decode(base64_decode($result['SecretBinary']), true));
+        } else {
+            echo "Unable to get email credentials from AWS Secrets Manager. Please run \"upload-email-credential.php\" if you have not done so already.\n";
+            get_local_email_config($callback);
         }
     } catch (AwsException $e) {
         echo "Unable to get email credentials from AWS Secrets Manager. Please run \"upload-email-credential.php\" if you have not done so already.\n" . $e->getMessage() . "\n" . $e->getAwsErrorMessage() . "\n" . $e->getAwsErrorCode() . "\n";
